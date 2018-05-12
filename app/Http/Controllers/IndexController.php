6c98cce89518d5a065624d710233c4fa12d8f890
root <root@localhost.localdomain>
2018-05-13 02:48:16 +0800

    init

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -9,9 +9,23 @@ class LoginController extends Controller
 {
     use AuthenticatesUsers;
 
-    //
+    /**
+     * @api {get} /list
+     * @apiName 获取列表
+     * @apiGroup Main
+     *
+     * @apiSuccess {String} firstname Firstname of the User.
+     * @apiSuccess {String} lastname  Lastname of the User.
+     *
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *       "firstname": "John",
+     *       "lastname": "Doe"
+     *     }
+     */
     public function list(){
-        
+
     }
 
     public function save(){
